25e99eb3c92569eef254dc8d62ffc988d2d50d11
Anthony Jones <anthonyjones0519@gmail.com>
2018-06-27 16:39:48 -0600

    gridster proof of concept, initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { wobble } from 'ng-animate';
+import {
+  GridsterConfig,
+  GridsterItem,
+  GridType,
+  DisplayGrid
+} from 'angular-gridster2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('wobble', [
+      transition(
+        '* => *',
+        useAnimation(wobble, {
+          params: { timing: 5 }
+        })
+      )
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  wobble: any;
   title = 'app';
+  options: GridsterConfig;
+  dashboard: Array<GridsterItem>;
+
+  static itemChange(item, itemComponent) {
+    console.info('itemChanged', item, itemComponent);
+  }
+
+  static itemResize(item, itemComponent) {
+    console.info('itemResized', item, itemComponent);
+  }
+
+  ngOnInit() {
+    this.options = {
+      gridType: GridType.Fit,
+      displayGrid: DisplayGrid.OnDragAndResize,
+      maxCols: 5,
+      maxRows: 5,
+      pushItems: true,
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      },
+
+      itemChangeCallback: AppComponent.itemChange,
+      itemResizeCallback: AppComponent.itemResize
+    };
+
+    this.loadGrid();
+  }
+
+  changedOptions() {
+    this.options.api.optionsChanged();
+  }
+
+  removeItem(item) {
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem() {
+    this.dashboard.push({ x: 0, y: 0, cols: 1, rows: 1 });
+  }
+
+  loadGrid() {
+    const storedDashboard = localStorage.getItem('dashboard');
+    this.dashboard = storedDashboard ? JSON.parse(storedDashboard) : [];
+  }
+
+  saveGrid() {
+    localStorage.setItem('dashboard', JSON.stringify(this.dashboard));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { GridsterModule } from 'angular-gridster2';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, GridsterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
